a41044563f09b9250ec31d82e06e757d26a0a7b3
ljh468 <ljh468@naver.com>
2021-06-16 06:34:23 +0900

    210616

--- selenium/service/seleniumService.py
+++ selenium/service/seleniumService.py
@@ -33,28 +33,26 @@ def seleniumCrawling():
         driver.implicitly_wait(15)  # 15초 대기
 
         list = []
-        for j in range(2, 4):
-            driver.find_element_by_xpath(
-                f'//*[@id="container"]/div[2]/div[1]/div[2]/div[3]/div/ul/li[{j}]/div[1]/a').click()
-            CONTEST_NAME = driver.find_element_by_xpath(
-                '//*[@id="container"]/div[2]/div[1]/div[2]/div/div[1]/h6').text  # 공모전 이름
-            # CONTEST_IMG = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/img').get_attribute('src') # 이미지 주소
-            # CONTEST_HOST = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[3]').text  # 주최
-            # CONTEST_AREA = (areaList[k])  # 분야
-            # CONTEST_DAY = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[5]').text  # 기간
-            # CONTEST_PRIZE = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[6]').text # 총상금
-            # CONTEST_ADDR = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[8]/a').get_attribute('href')  # 홈페이지주소
-            # CONTEST_CONTENTS = driver.find_element_by_css_selector('#viewContents').get_attribute('innerHTML') # 공모전 내용
+        for j in range(2, 12):
+            driver.find_element_by_xpath(f'//*[@id="container"]/div[2]/div[1]/div[2]/div[3]/div/ul/li[{j}]/div[1]/a').click()
+            CONTEST_NAME = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[1]/h6').text  # 공모전 이름
+            CONTEST_IMG = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/img').get_attribute('src') # 이미지 주소
+            CONTEST_HOST = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[3]').text  # 주최
+            CONTEST_AREA = (areaList[k])  # 분야
+            CONTEST_DAY = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[5]').text  # 기간
+            CONTEST_PRIZE = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[6]').text # 총상금
+            CONTEST_ADDR = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/ul/li[8]/a').get_attribute('href')  # 홈페이지주소
+            CONTEST_CONTENTS = driver.find_element_by_css_selector('#viewContents').get_attribute('innerHTML') # 공모전 내용
 
             ContestDTO = {}  # object
             ContestDTO['contest_name'] = CONTEST_NAME
-            # ContestDTO['contest_img'] = CONTEST_IMG
-            # ContestDTO['contest_host'] = CONTEST_HOST
-            # ContestDTO['contest_area'] = CONTEST_AREA # 분야
-            # ContestDTO['contest_day'] = CONTEST_DAY
-            # ContestDTO['contest_prize'] = CONTEST_PRIZE
-            # ContestDTO['contest_addr'] = CONTEST_ADDR
-            # ContestDTO['contest_contents'] = CONTEST_CONTENTS
+            ContestDTO['contest_img'] = CONTEST_IMG
+            ContestDTO['contest_host'] = CONTEST_HOST
+            ContestDTO['contest_area'] = CONTEST_AREA # 분야
+            ContestDTO['contest_day'] = CONTEST_DAY
+            ContestDTO['contest_prize'] = CONTEST_PRIZE
+            ContestDTO['contest_addr'] = CONTEST_ADDR
+            ContestDTO['contest_contents'] = CONTEST_CONTENTS
 
             list.append(ContestDTO)
             # print("CONTEST_NAME : ", CONTEST_NAME)
@@ -69,6 +67,6 @@ def seleniumCrawling():
             driver.back()
         rList['rlist'] = list
         wList.append(rList)
-    k = k + 1
+        k = k + 1
     print(wList)
     return wList
